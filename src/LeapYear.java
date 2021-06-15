c94003f97eb9dada57728f4ec54b5a6323337ac1
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-06-15 18:52:41 +0530

    [Javeed] Added LeapYear Or NOT

---
+++ src/LeapYear.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class LeapYear{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year;
+		System.out.print("enter the year = ");
+		year = sc.nextInt();
+
+		if ( (year % 4 == 0) && (year % 100 == 0) && ( year % 400 == 0) ) {
+				System.out.println(year+" is Leap year ");
+			}
+		else
+			System.out.println(year+" is not a Leap year");
+	sc.close();
+	}
+	}
